951c30f83481afa2f5cb75962c1b28b9dbd4e4f2
rjbultitude <richard.bultitude@gmail.com>
2015-01-24 15:18:22 +0000

    style up cta buttons. create default duration and amend transitions. add item to z-index list

--- includes/cta-icon-join.php
+++ includes/cta-icon-join.php
@@ -1,7 +1,7 @@
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 58 58" enable-background="new 0 0 58 58" xml:space="preserve">
-<circle fill="#FFFFFF" cx="29" cy="29" r="29"/>
-<g>
-	<path fill="#3D4F2D" d="M15.8,26.4l9.7,0.1v-10h5.5v10l9.8,0.1v5.1l-9.8,0v10.6h-5.5V31.7l-9.7,0V26.4z"/>
-</g>
+	<circle fill="#FFFFFF" cx="29" cy="29" r="29"/>
+	<g>
+		<path fill="#3D4F2D" d="M15.8,26.4l9.7,0.1v-10h5.5v10l9.8,0.1v5.1l-9.8,0v10.6h-5.5V31.7l-9.7,0V26.4z"/>
+	</g>
 </svg>
